ee848e382afdd5c23eacf04ede321d2352dddf8b
Yusuke Endoh <mame@ruby-lang.org>
2022-12-01 12:26:45 +0900

    Update the API server

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
 (function () {
-  const API_ENDPOINT = "https://aluminium.ruby-lang.org/typeprof-playground";
+  const API_ENDPOINT = "https://mame.dev.ruby-lang.org/typeprof-playground/";
 
   const $btnAnalyze = document.getElementById("btn-analyze");
   const $btnReportBug = document.getElementById("btn-report-bug");
